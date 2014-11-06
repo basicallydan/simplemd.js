27cf625eecfb6f6584eeeaf0d8c97765a33e0d12
Dan Hough <daniel.hough@gmail.com>
2014-11-06 11:46:42 +0000

    OK Genuinely got tests passing now

--- tests.js
+++ tests.js
@@ -8,19 +8,32 @@ function assertOutputEqual(func, input, expectedOutput) {
 	++testNum;
 	try {
 		output = func(input);
-		console.assert(output === expectedOutput, 'Equality test', testNum, 'failed:', input, '!==', expectedOutput);
+		console.assert(output === expectedOutput, 'Equality test', testNum, 'failed:');
 	} catch (e) {
-
+		console.log('-- Test', testNum, 'FAILED --');
+		console.log(output + '\n!==\n' + expectedOutput);
+		return console.log('\n');
+		// return console.error(e);
 	}
-	console.log('Test', testNum, 'passed:');
+	console.log('-- Test', testNum, 'passed --');
 	console.log(output);
 	console.log('\n');
 }
 
-assertOutputEqual(parseMarkdown, 'This is a paragraph\n\nThis is another paragraph.', '<p>This is a paragraph</p>\n<p>This is another paragraph.</p>');
+assertOutputEqual(parseMarkdown,
+	'This is a paragraph\n\nThis is another paragraph.',
+	'<p>This is a paragraph</p>\n<p>This is another paragraph.</p>');
 assertOutputEqual(parseMarkdown, '# Header 1', '<h1>Header 1</h1>');
-assertOutputEqual(parseMarkdown, '## Header 2', '<h2>Header 1</h2>');
-assertOutputEqual(parseMarkdown, '#Header 3\n\nThis is a paragraph\n\nThis is another paragraph.', '<h1>Header 1</h1>\n<p>This is a paragraph</p>\n<p>This is another paragraph.</p>');
-assertOutputEqual(parseMarkdown, '#Header 3\n\nThis is a paragraph\n\n####Header 4\n\nThis is another paragraph.', '<h1>Header 1</h1>\n<p>This is a paragraph</p>\n<h4>Header 4</h4>\n<p>This is another paragraph.</p>');
-assertOutputEqual(parseMarkdown, '#Header 3\n\nThis is a paragraph\n\n####Header 4\n\nThis is [a link](http://google.com).', '<h1>Header 1</h1>\n<p>This is a paragraph</p>\n<h4>Header 4</h4>\n<p>This is <a href="http://google.com">a link</a>.</p>');
-assertOutputEqual(parseMarkdown, '#Header 3\n\nThis [has](http://google.com) [some](http://reddit.com) [links](http://trello.com)\n\n####Header 4', '<h1>Header 1</h1>\n<p>This <a href="http://google.com">has</a> <a href="http://reddit.com">some</a> <a href="http://trello.com">links</a></p>\n<h4>Header 4</h4>');
+assertOutputEqual(parseMarkdown, '## Header 2', '<h2>Header 2</h2>');
+assertOutputEqual(parseMarkdown,
+	'#Header 1\n\nThis is a paragraph\n\nThis is another paragraph.',
+	'<h1>Header 1</h1>\n<p>This is a paragraph</p>\n<p>This is another paragraph.</p>');
+assertOutputEqual(parseMarkdown,
+	'#Header 1\n\nThis is a paragraph\n\n####Header 4\n\nThis is another paragraph.',
+	'<h1>Header 1</h1>\n<p>This is a paragraph</p>\n<h4>Header 4</h4>\n<p>This is another paragraph.</p>');
+assertOutputEqual(parseMarkdown,
+	'#Header 1\n\nThis is a paragraph\n\n####Header 4\n\nThis is [a link](http://google.com).',
+	'<h1>Header 1</h1>\n<p>This is a paragraph</p>\n<h4>Header 4</h4>\n<p>This is <a href="http://google.com">a link</a>.</p>');
+assertOutputEqual(parseMarkdown,
+	'#Header 1\n\nThis [has](http://google.com) [some](http://reddit.com) [links](http://trello.com)\n\n####Header 4',
+	'<h1>Header 1</h1>\n<p>This <a href="http://google.com">has</a> <a href="http://reddit.com">some</a> <a href="http://trello.com">links</a></p>\n<h4>Header 4</h4>');
